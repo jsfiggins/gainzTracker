04658a182c4ff76f853f5561715b77fa38d44701
Jaseane Figgins <figginsjaseane@gmail.com>
2024-08-30 22:09:19 -0400

    updates

--- client/gainzTracker/vite.config.js
+++ client/gainzTracker/vite.config.js
@@ -1,11 +1,11 @@
-// vite.config.js
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: 4173,      // Ensure this matches the port Render expects (you can choose a different port if needed)
     proxy: {
       '/api': {
         target: 'http://localhost:7000', // Update this to match your backend server's address
