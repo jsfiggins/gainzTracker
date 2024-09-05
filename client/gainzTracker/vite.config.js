65c654f73be5ea2f81cff63caaabc5a51f6ede66
Jaseane Figgins <figginsjaseane@gmail.com>
2024-09-05 13:17:23 -0400

    port update

--- client/gainzTracker/vite.config.js
+++ client/gainzTracker/vite.config.js
@@ -1,17 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// vite.config.js
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: 7000,      // Ensure this matches the port Render expects (you can choose a different port if needed)
     proxy: {
       '/api': {
         target: 'https://gainztracker-uc6w.onrender.com',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, '')
       },
     },
+      
   },
 });
