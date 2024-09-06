a68da91ae304606dce6dc70dd4aad662b37d3ca9
Jaseane Figgins <figginsjaseane@gmail.com>
2024-09-06 12:41:24 -0400

    yes

--- client/gainzTracker/vite.config.js
+++ client/gainzTracker/vite.config.js
@@ -3,13 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root:'./gainzTracker',
+  root: './', // This tells Vite to use the current directory as the root
+  build: {
+    outDir: 'dist' // Specify your build output directory
+  },
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:7000', // Backend server
         changeOrigin: true,
-    
       }
     }
   }
