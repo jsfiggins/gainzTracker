8f8e513157bf8b1a7ba10c2388de28fbcbfebf36
Jaseane Figgins <figginsjaseane@gmail.com>
2024-09-11 15:35:19 -0400

    config

--- client/gainzTracker/vite.config.js
+++ client/gainzTracker/vite.config.js
@@ -1,23 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    root: 'client/gainzTracker', // This should point to where your index.html is
-    server: {
-      proxy: mode === 'development' ? {
-        '/api': {
-          target: 'http://localhost:7000',
-          changeOrigin: true,
-        }
-      } : undefined,
-    },
-    build: {
-      outDir: 'dist', // Outputs to client/gainzTracker/dist
-      rollupOptions: {
-        input: 'client/gainzTracker/index.html', // This ensures Rollup knows where the entry point is
-      },
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:7000', // Backend server
+        changeOrigin: true,
+    
+      }
+    }
+  }
 });
